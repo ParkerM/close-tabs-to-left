2a962526d77eec914ec51a4f678b4fc821750de6
Parker Mauney <parkermauney@gmail.com>
2020-02-05 17:53:28 -0600

    Add working prototype; remove old files

--- src/background.js
+++ src/background.js
@@ -1,28 +1,26 @@
-function updateCount(tabId, isOnRemoved) {
-  browser.tabs.query({})
-  .then((tabs) => {
-    let length = tabs.length;
-
-    // onRemoved fires too early and the count is one too many.
-    // see https://bugzilla.mozilla.org/show_bug.cgi?id=1396758
-    if (isOnRemoved && tabId && tabs.map((t) => { return t.id; }).includes(tabId)) {
-      length--;
-    }
-
-    browser.browserAction.setBadgeText({text: length.toString()});
-    if (length > 2) {
-      browser.browserAction.setBadgeBackgroundColor({'color': 'green'});
-    } else {
-      browser.browserAction.setBadgeBackgroundColor({'color': 'red'});
-    }
-  });
+function onCreated() {
+  if (browser.runtime.lastError) {
+    console.log(`Error: ${browser.runtime.lastError}`);
+  } else {
+    console.log("Item created successfully");
+  }
 }
 
+function closeTabsWithIdxLessThan(tabIdx) {
+  for (let i = 0; i < tabIdx; i++) {
+    browser.tabs.query({index: i, currentWindow: true})
+        .then(tabs => browser.tabs.remove(tabs[0].id))
+  }
+}
 
-browser.tabs.onRemoved.addListener(
-  (tabId) => { updateCount(tabId, true);
-});
-browser.tabs.onCreated.addListener(
-  (tabId) => { updateCount(tabId, false);
-});
-updateCount();
+function closeTabsToLeft(info, tab) {
+  closeTabsWithIdxLessThan(tab.index);
+}
+
+browser.menus.create({
+  id: "tab-close-to-left",
+  type: "normal",
+  contexts: ["tab"],
+  title: "Close Tabs to the Left",
+  onclick: closeTabsToLeft
+}, onCreated);
