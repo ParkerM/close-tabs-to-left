374ac0456640b919ab43adcba3d2dacc0ae92917
Parker Mauney <parkermauney@gmail.com>
2020-06-13 15:10:02 -0500

    Disable button when there aren't any tabs to the left

--- src/lifecycle.ts
+++ src/lifecycle.ts
@@ -1,10 +1,17 @@
 import {browser, Menus, Tabs} from 'webextension-polyfill-ts';
+import {MENU_ITEM_ID, minimizingIndex} from './util';
+import Tab = Tabs.Tab;
+import OnClickData = Menus.OnClickData;
+import OnShownInfoType = Menus.OnShownInfoType;
 
 export class Lifecycle {
-  init(onclick: (info: Menus.OnClickData, target: Tabs.Tab) => void): number | string {
-    return browser.menus.create(
+  private static lastMenuInstanceId = 0;
+  private static nextMenuInstanceId = 1;
+
+  init(onclick: (info: OnClickData, target: Tab) => void): void {
+    browser.menus.create(
       {
-        id: 'tab-close-to-left',
+        id: MENU_ITEM_ID,
         type: 'normal',
         contexts: ['tab'],
         title: 'Close Tabs to the Left',
@@ -15,10 +22,44 @@ export class Lifecycle {
   }
 
   onCreated(): void {
-    if (browser.runtime.lastError) {
-      console.error(`Error: ${browser.runtime.lastError}`);
-    } else {
-      console.debug('Item created successfully');
-    }
+    browser.menus.onShown.addListener(Lifecycle.updateEnabledState);
+    browser.menus.onHidden.addListener(Lifecycle.resetMenuInstanceState);
   }
+
+  /**
+   * Toggles the enabled state of the Close Tabs to the Left button.
+   * If targetTab has the same index as the leftmost tab in the current window, the menu button is disabled.
+   * Otherwise, the menu button is enabled.
+   *
+   * @see https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/menus/onShown
+   */
+  private static updateEnabledState: (info: OnShownInfoType, targetTab: Tab | undefined) => void = async (
+    info,
+    targetTab,
+  ) => {
+    // If targetTab is undefined then this is not a tab menu
+    if (!targetTab) return;
+
+    const menuInstanceId = Lifecycle.nextMenuInstanceId++;
+    Lifecycle.lastMenuInstanceId = menuInstanceId;
+
+    const leftmostTabInCurrentWindow: Tab = await browser.tabs
+      .query({currentWindow: true})
+      .then(windowTabs => windowTabs.reduce(minimizingIndex));
+
+    // Verify that menu was not closed and shown again during async operation
+    if (menuInstanceId !== Lifecycle.lastMenuInstanceId) return;
+
+    // noinspection ES6MissingAwait
+    browser.menus.update(MENU_ITEM_ID, {
+      enabled: leftmostTabInCurrentWindow.index !== targetTab.index,
+    });
+    // noinspection ES6MissingAwait
+    browser.menus.refresh();
+  };
+
+  /**
+   * Reset the menu open/closed state used by the onShown handler.
+   */
+  private static resetMenuInstanceState = () => (Lifecycle.lastMenuInstanceId = 0);
 }
